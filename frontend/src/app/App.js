545958df65dcd3a2d66db711b480d37ef5a2cae2
Daniil_Borovoy <74528634+Daniel-Borovoy@users.noreply.github.com>
2021-10-22 22:08:50 +0300

    change project structure

---
+++ frontend/src/app/App.js
@@ -0,0 +1,12 @@
+import logo from '../logo.svg'
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <div className={styles.logo}>
+      <img  width={100} src={logo} alt='logo'></img>
+    </div>
+  )
+}
+
+export default App;
