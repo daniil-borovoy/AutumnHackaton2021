45229f649b9de9363aaa69c3d9bf6fb24b7fbdbb
Artem Pavlenko <artempavlenko1703@gmail.com>
2021-10-23 09:18:51 +0300

    card, btn and transaction

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,12 +1,26 @@
+import { Button, Container } from '@material-ui/core';
 import logo from '../logo.svg'
 import styles from './App.module.scss'
+import BoxCard from './card/Card';
+import Transaction from './transaction/Transaction';
+
 
 function App() {
-  return (
-    <div className={styles.logo}>
-      <img  width={100} src={logo} alt='logo'></img>
-    </div>
-  )
+	return (
+		<div className={styles.logo}>
+			<img width={100} src={logo} alt='logo'></img>
+			<Container maxWidth="xs">
+				<BoxCard endCard="12/24" numberCard="8800" typeCard="Platinume" balanceCard="10 230 000 P" />
+				<Button
+					variant='contained'
+					className={styles.button}
+				>
+					История операций
+				</Button>
+				<Transaction nameTransaction="Альфа-банк" sphereTransaction="еда" valueTransaction="18 829 р" />
+			</Container>
+		</div>
+	)
 }
 
 export default App;
