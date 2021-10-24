503c4d30759827beba063aff5570f8e639dd9dfe
Daniil_Borovoy <74528634+Daniel-Borovoy@users.noreply.github.com>
2021-10-24 15:47:36 +0300

    refactor

--- frontend/src/app/chart/Chart.js
+++ frontend/src/app/chart/Chart.js
@@ -3,21 +3,17 @@ import { ResponsiveStream } from '@nivo/stream'
 import { useState } from 'react'
 import MCC from '../mcc.json'
 import styles from './Chart.module.scss'
-// import Skeleton from '@material-ui/lab/Skeleton'
-// import { makeStyles } from "@material-ui/core/styles"
 let _ = require('lodash')
 const months = [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь","Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ]
-
 export const mccDecoder = (mcc) => {
   for (let i = 0; i < MCC.length; i++) {
     if ( MCC[i].mcc === mcc) {
-      // console.log("asdad",MCC[i].edited_description)
       return MCC[i].edited_description
     }
   }
 }
 
-const calcValue = (tranzaktions) => { // сумма группы MCC
+const calcValue = (tranzaktions) => { // сумма в категории (mcc) за месяцы
   if (tranzaktions) {
   let s = 0
   for (let i = 0; i < tranzaktions.length; i++){
@@ -39,11 +35,9 @@ const dateConverter = (data, date) => {
       data[i].monthNumber = tranDate.getUTCMonth()
     } 
   }
-  // console.log(_.groupBy(data, 'monthNumber'), date)
   return _.groupBy(data, 'monthNumber')[date]
 }
 const categorySumm = (data, mcc) => {
-  // const withoutSymbolLength = Object.keys(data).length
   let s = 0
   for (let i = 1; i < mcc.length; i++) {
     for (let j = 0; j < data[mcc[i]].length; j++) {
@@ -52,28 +46,6 @@ const categorySumm = (data, mcc) => {
   } return s
 }
 
-
-// const other = (convertData, mcc, summ, sortArray) => { 
-//   let othermcc = [] // категрия прочее
-//   for (let i = 0; i < mcc.length; i++) {
-//     if (convertData[mcc[i]]) {
-//       if ( ( (calcValue(convertData[mcc[i]]))/summ)*100 <= 10) {
-//         console.log(`${mccDecoder(mcc[i]) } меньше чем 10%`)
-//         othermcc.push(mcc[i])
-//       }
-//     }
-    
-//   }
-//   sortArray.push(
-//     {
-//       'id': 'Прочее',
-//       'label': '',
-//       'mcc': othermcc,
-//       'value': categorySumm(convertData, othermcc),
-//       'color': 'hsl(85, 70%, 50%)'
-//     })
-// }
-
 const dataConverterPercent = (data, date) => {
   let convertData = []
   let monthsData = []
@@ -81,10 +53,7 @@ const dataConverterPercent = (data, date) => {
   for (let i = 0; i < 12; i++) {
     
     convertData = dateConverter(data, i) // сортируем по дате
-    // console.log(convertData)
-    // console.log(convertData)
     convertData = _.groupBy(convertData, 'MCC') // сортируем по типу транзакции
-    // console.log('SECOND',convertData)
     const mcc = Object.keys(convertData)
     const summMonth = categorySumm(convertData, mcc)
     let a = []
@@ -99,19 +68,8 @@ const dataConverterPercent = (data, date) => {
     if (a.length) {
       All.push(a)
     }
-    // console.log(a)
   }
-
-
-  console.log(All)
-
-  let gArray = [
-  ] 
-  //      "5814": 10,
-      // "5999": 10,
-      // "6011": 10,
-      // "6012": 50,
-      // "7374": 20
+  let gArray = []
   for (let i = 0; i < 8; i++) {
     gArray.push(
       {
@@ -123,35 +81,19 @@ const dataConverterPercent = (data, date) => {
       }
     )
   }
-  console.log(gArray)
-  // {
-  //   "Фастфуд": 0,
-  //   "Розничные магазины": 0,
-  //   "Снятие наличных": 0,
-  //   "Торговля и услуги": 0,
-  //   "Программирование": 0
-  // }
   return gArray
-  // console.log(calcValue(convertData['6011']))
 }
 
 let convertData = []
 const dataConverter = (data, type, period, date) => {
 
   let sortArray = []
-
-  // console.log('FIRST',convertData ,data)
   convertData = dateConverter(data, date) // сортируем по дате
-  // console.log('SECOND',convertData)
   convertData = _.groupBy(convertData, 'MCC') // сортируем по типу транзакции
   const mcc = Object.keys(convertData) // определяем номера транзацкций
-  // console.log(convertData)
-  // let summ = categorySumm(convertData, mcc) // сумма всех транзакций за период
-  // const mcc2 = [6011, 6012] // WIP
-  // console.log("MCC",mcc, convertData[mcc[2]])
   // заполняем сортированный массив
   for (let i = 0; i < mcc.length; i++) {
-    if(mcc[i] === '0') { // пропускаем INSIDE транзакции
+    if(mcc[i] === '0') { // временно пропускаем INSIDE транзакции
       continue
     }
       sortArray.push({
@@ -162,8 +104,6 @@ const dataConverter = (data, type, period, date) => {
         'color': 'hsl(85, 70%, 50%)'
       })
   }
-  // определяем набиолее мелкие затраты (10% от всей суммы) и заносим в группу 'Прочие' и сразу в сортированный массив
-  // other(convertData, mcc, summ, sortArray)
   if (sortArray.length) {
     return sortArray
   } return []
@@ -178,17 +118,17 @@ const Chart = ({ data }) => {
   const [chartType, setChartType] = useState('expenses')
   const [date, setDate] = useState(initialDate.getUTCMonth())
   dataConverterPercent(data, date)
+
   if (data) {
     return (
     <div className={styles.chart}>
       {chartType === 'expenses' ? (
         <div className={styles.timeChange}>
-        <button className={styles.decrement} onClick={() => setDate(date > 0 ? date - 1 : date)}><svg width={15}  style={{transform: 'rotate(180deg)'}} viewBox="0 0 5 9">
-        <path d="M0.419,9.000 L0.003,8.606 L4.164,4.500 L0.003,0.394 L0.419,0.000 L4.997,4.500 L0.419,9.000 Z" />
+        <button className={styles.decrement} onClick={() => setDate(date > 0 ? date - 1 : date)}><svg width={15}  style={{transform: 'rotate(180deg)'}} viewBox='0 0 5 9'>
+        <path d='M0.419,9.000 L0.003,8.606 L4.164,4.500 L0.003,0.394 L0.419,0.000 L4.997,4.500 L0.419,9.000 Z' />
         </svg></button>
         <ResponsivePie
             style={{position: 'relative'}}
-            // valueFormat=""
             data={dataConverter(data, type, period, date)}
             margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
             enableArcLabels={false}
@@ -201,87 +141,90 @@ const Chart = ({ data }) => {
             borderWidth={2}
             borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
             arcLabelsSkipAngle={10}
-        ></ResponsivePie>
-        <h2 className={styles.date}>{months[date]} 2021</h2>
-        {dataConverter(data, type, period, date).length ? <p className={styles.summ}>{categorySumm(convertData, Object.keys(convertData))} руб.</p> : <p className={styles.expenses}>Расходы отсутвтуют</p>}
-      
-        <button className={styles.increment} onClick={() => setDate(date < 11 ? date + 1 : date)}><svg width={15}  viewBox="0 0 5 9">
-        <path d="M0.419,9.000 L0.003,8.606 L4.164,4.500 L0.003,0.394 L0.419,0.000 L4.997,4.500 L0.419,9.000 Z" /></svg></button>
-        </div>)
-        :
-        (<div className={styles.streamGraph}>
-        <ResponsiveStream
-        data={dataConverterPercent(data)}
-        keys={[ 'Фастфуд', 'Розничные магазины', 'Снятие наличных', 'Торговля и услуги', 'Программирование']}
-        margin={{ top: 50, right: 60, bottom: 50, left: 60 }}
-        offsetType="expand"
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            orient: 'bottom',
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: '',
-            legendOffset: 36
-        }}
-        enableDots={true}
-        axisLeft={{ orient: 'left', tickSize: 5, tickPadding: 5, tickRotation: 0, legend: '', legendOffset: -40 }}
-        enableGridX={true}
-        enableGridY={false}
-        offsetType="silhouette"
-        colors={{ scheme: 'nivo' }}
-        fillOpacity={0.85}
-        borderColor={{ theme: 'background' }}
-        defs={[
-            {
-                id: 'dots',
-                type: 'patternDots',
-                background: 'inherit',
-                color: '#2c998f',
-                size: 4,
-                padding: 2,
-                stagger: true
-            },
-            {
-                id: 'squares',
-                type: 'patternSquares',
-                background: 'inherit',
-                color: '#e4c912',
-                size: 6,
-                padding: 2,
-                stagger: true
-            }
-        ]}
-        fill={[
-            {
-                match: {
-                    id: 'Paul'
+        />
+          <h2 className={styles.date}>{months[date]} 2021</h2>
+          {dataConverter(data, type, period, date).length ? <p className={styles.summ}>{categorySumm(convertData, Object.keys(convertData))} руб.</p> : <p className={styles.expenses}>Расходы отсутвтуют</p>}
+          <button className={styles.increment} onClick={() => setDate(date < 11 ? date + 1 : date)}><svg width={15}  viewBox="0 0 5 9">
+          <path d="M0.419,9.000 L0.003,8.606 L4.164,4.500 L0.003,0.394 L0.419,0.000 L4.997,4.500 L0.419,9.000 Z" /></svg></button>
+          </div>
+        ):
+        (
+          <div className={styles.streamGraph}>
+            <ResponsiveStream
+            data={dataConverterPercent(data)}
+            keys={[ 'Фастфуд', 'Розничные магазины', 'Снятие наличных', 'Торговля и услуги', 'Программирование']}
+            margin={{ top: 50, right: 60, bottom: 50, left: 60 }}
+            offsetType="expand"
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                orient: 'bottom',
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: '',
+                legendOffset: 36
+            }}
+            enableDots={true}
+            axisLeft={{ orient: 'left', tickSize: 5, tickPadding: 5, tickRotation: 0, legend: '', legendOffset: -40 }}
+            enableGridX={true}
+            enableGridY={false}
+            offsetType="silhouette"
+            colors={{ scheme: 'nivo' }}
+            fillOpacity={0.85}
+            borderColor={{ theme: 'background' }}
+            defs={[
+                {
+                    id: 'dots',
+                    type: 'patternDots',
+                    background: 'inherit',
+                    color: '#2c998f',
+                    size: 4,
+                    padding: 2,
+                    stagger: true
                 },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'Marcel'
+                {
+                    id: 'squares',
+                    type: 'patternSquares',
+                    background: 'inherit',
+                    color: '#e4c912',
+                    size: 6,
+                    padding: 2,
+                    stagger: true
+                }
+            ]}
+            fill={[
+                {
+                    match: {
+                        id: 'Paul'
+                    },
+                    id: 'dots'
                 },
-                id: 'squares'
-            }
-        ]}
-        dotSize={8}
-        dotColor={{ from: 'color' }}
-        dotBorderWidth={2}
-        dotBorderColor={{ from: 'color', modifiers: [ [ 'darker', 0.7 ] ] }}
-    />
-
-        </div>)
-    
+                {
+                    match: {
+                        id: 'Marcel'
+                    },
+                    id: 'squares'
+                }
+            ]}
+            dotSize={8}
+            dotColor={{ from: 'color' }}
+            dotBorderWidth={2}
+            dotBorderColor={{ from: 'color', modifiers: [ [ 'darker', 0.7 ] ] }}/>
+          </div>
+        )
       }
       <div className={styles.buttons}>
         <div className={styles.chartTypes}>
-          <button className={type === 'expenses' ? styles.active : ''} onClick={() => {setType('expenses')
-        setChartType('expenses')}}>Расходы</button>
-          <button className={type === 'comparison' ? styles.active : ''} onClick={() => {setType('comparison')
-        setChartType('comparison')}}>Сравнение расходов</button>
+          <button className={type === 'expenses' ? styles.active : ''} onClick={() => 
+          {
+            setType('expenses')
+            setChartType('expenses')
+          }}>Расходы</button>
+          <button className={type === 'comparison' ? styles.active : ''} onClick={() => {
+            setType('comparison')
+            setChartType('comparison')
+          }}>Сравнение расходов</button>
         </div>
         <div className={styles.chartPeriods}>
           <button className={period === 'year' ? styles.active : ''} onClick={() => setPeriod('year')}>Год</button>
